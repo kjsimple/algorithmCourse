37c895b25821f0cfea8b41f72e42577a29727905
kjsimple <kjsimple@gmail.com>
2011-05-09 13:41:47 +0800

    minor fix, rename kapsack.groovy to Knapsack.groovy

---
+++ src/Knapsack.groovy
@@ -0,0 +1,31 @@
+
+/**
+ *
+ */
+
+def v = 50
+def objs = [2, 4, 18, 17, 7, 14, 9, 33, 21]
+def res = []
+
+0.upto(v) {
+    res [it] = 0
+}
+
+0.upto(v) {
+    it = it.toString().center(2)
+    println "$it "
+}
+println '***'
+
+objs.each { obj ->
+    v.downto(0) { currentV ->
+        if (currentV - obj >= 0) {
+            res[currentV] = Math.max(res[currentV], res[currentV - obj] + obj)
+        }
+    }
+    res.each {
+        it = it.toString().center(2)
+        print "$it "
+    }
+    println ''
+}
