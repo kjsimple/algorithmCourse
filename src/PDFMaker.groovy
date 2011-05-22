fd24955fadc44716a5e95a4abc29980133c3f578
kjsimple <kjsimple@gmail.com>
2011-05-22 14:35:37 +0800

    pdfmaker

--- src/PDFMaker.groovy
+++ src/PDFMaker.groovy
@@ -1,5 +1,5 @@
 import com.itextpdf.text.Document
-import com.itextpdf.text.Jpeg
+import com.itextpdf.text.Image
 import com.itextpdf.text.Rectangle
 import com.itextpdf.text.pdf.PdfWriter
 
@@ -7,17 +7,19 @@ import com.itextpdf.text.pdf.PdfWriter
  *
  */
 
-def foldName = 'd:/temp/swyx'
+def foldName = 'd:/temp/hfjj'
+def dest = 'd:/test.pdf'
+
 fileNames = []
 new File(foldName).eachFile {
     fileNames.add(it.name)
 }
 
 def doc = new Document();
-PdfWriter.getInstance(doc, new FileOutputStream("d:/test.pdf"))
-doc.setPageSize(new Rectangle(699, 1034))
+PdfWriter.getInstance(doc, new FileOutputStream(dest))
+doc.setPageSize(new Rectangle(991, 1439))
 doc.open()
 fileNames.sort({o1, o2-> o1.compareTo(o2)}).each {
-    doc.add(new Jpeg(new URL("file:///"+new File('d:/temp/swyx', it).canonicalPath)))
+    doc.add(Image.getInstance(new File(foldName, it).canonicalPath))
 }
 doc.close()
